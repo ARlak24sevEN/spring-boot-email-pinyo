efeccf4450af8f6f0047d779579bd532065665c1
CsignS <smileroom2010@gmail.com>
2021-11-03 17:54:14 +0700

    Add send email and Add schedule

--- src/main/java/com/iamnbty/training/backend/BackendApplication.java
+++ src/main/java/com/iamnbty/training/backend/BackendApplication.java
@@ -2,8 +2,12 @@ package com.iamnbty.training.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+import javax.persistence.Embeddable;
 
 @SpringBootApplication
+@EnableScheduling
 public class BackendApplication {
 
     public static void main(String[] args) {
